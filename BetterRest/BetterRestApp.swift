5bb5a6e900a5084db75752a348715b962e7b58c0
KrasnovYuri <krasnov1yuri@gmail.com>
2024-02-11 19:18:56 +0300

    add project

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Юрий on 11.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
